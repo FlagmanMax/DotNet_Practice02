9de289e0f5f1264b53310cf391fd851fcae896f2
borodov.m <borodov.m@gmail.com>
2022-09-20 23:53:36 +0300

    Added example 14

---
+++ Example_014/Program.cs
@@ -0,0 +1,26 @@
+﻿// 14. Напишите программу, которая принимает на
+// вход число и проверяет, кратно ли оно
+// одновременно 7 и 23.
+// 14 -> нет
+// 46 -> нет
+// 161 -> да
+
+void ex14()
+{
+    Console.Write("Число = ");
+    int val1 = Convert.ToInt32(Console.ReadLine());
+
+
+    int result = val1 % (7*23);
+
+    if (result == 0)
+    {
+        Console.WriteLine("Кратно");
+    }
+    else
+    {
+        Console.WriteLine("НЕ кратно, ");
+    }
+}
+
+ex14();
